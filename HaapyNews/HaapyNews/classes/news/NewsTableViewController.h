f86783db66d007c6ca76d070f52d9c5e01a224c8
WZY <275886015@qq.com>
2015-10-01 09:21:53 +0800

    准备工作

---
+++ HaapyNews/HaapyNews/classes/news/NewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewController.h
+//  HaapyNews
+//
+//  Created by 甲丁乙_ on 15/10/1.
+//  Copyright © 2015年 wzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewController : UITableViewController
+
+@end
